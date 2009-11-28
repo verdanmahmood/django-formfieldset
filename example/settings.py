b1032b4a09bd6d996d15edccab18cd0f15b140a3
Atamert Ölçgen <muhuk@muhuk.com>
2009-11-28 13:11:30 +0200

    new example project

---
+++ example/settings.py
@@ -0,0 +1,27 @@
+import os
+
+
+_PATH = os.path.abspath(os.path.dirname(__file__))
+_MODULE = os.path.basename(_PATH)
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASE_ENGINE = 'sqlite3'
+
+ROOT_URLCONF = _MODULE + '.urls'
+
+MEDIA_ROOT = os.path.join(_PATH, 'media')
+MEDIA_URL = '/media/'
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+
+TEMPLATE_DIRS = (
+    os.path.join(_PATH, 'templates'),
+)
+
+INSTALLED_APPS = (
+    'formfieldset',
+)
+
+SECRET_KEY = '#s$zy-6goa)#&bn5)z!)ky+=295(x)pj!_p34n(1z_8_xm55no'
