b14f163c14e87eb61af2d203a2df9731203b4ee8
Atamert Ölçgen <muhuk@muhuk.com>
2009-12-05 12:52:19 +0200

    Basic example

--- example/examples/forms/basic.py
+++ example/examples/forms/basic.py
@@ -1,5 +1,19 @@
 from django import forms
+from formfieldset.forms import FieldsetMixin
 
 
-class ExampleForm(forms.Form):
-    foo = forms.CharField()
+class ExampleForm(forms.Form, FieldsetMixin):
+    """Your basic contact form"""
+    fieldsets = (
+        (u'Personal Info', {'fields': ('your_name',
+                                       'email_address',
+                                       'homepage')}),
+        (u'Message', {'fields': ('your_message',)}),
+        (u'Options', {'fields': ('send_a_copy_to_me',)}),
+    )
+
+    your_name = forms.CharField()
+    email_address = forms.EmailField()
+    homepage = forms.URLField()
+    your_message = forms.CharField(widget=forms.widgets.Textarea)
+    send_a_copy_to_me = forms.BooleanField()
