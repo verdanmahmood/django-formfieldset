7dcb4ed3d8ec707779b9e0aa9efb5dc5a83cebca
Atamert Ölçgen <muhuk@muhuk.com>
2009-12-20 21:49:18 +0200

    distutils doesn't accept unicode for `author`, UTF8 works fine

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 from formfieldset import __version__, __maintainer__, __email__
 
+
 license_text = open('LICENSE.txt').read()
 long_description = open('README.rst').read()
 
@@ -9,7 +10,7 @@ setup(
     name = 'django-formfieldset',
     version = __version__,
     url = 'http://github.com/muhuk/django-formfieldset/tree/master',
-    author = __maintainer__,
+    author = __maintainer__.encode('utf8'),
     author_email = __email__,
     license = license_text,
     packages = ['formfieldset'],
